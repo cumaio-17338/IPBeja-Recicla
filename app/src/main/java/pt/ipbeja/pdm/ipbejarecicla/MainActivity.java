d2751339cbca178717ef10171b412421c34404dd
LeonilSulude <leonilchaplin@gmail.com>
2021-02-01 16:52:34 +0000

    Adição das atividades do impacto - Acertar cardviews meu impacto, Statistics layout organizado

--- app/src/main/java/pt/ipbeja/pdm/ipbejarecicla/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm/ipbejarecicla/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity{
         impacts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivivty.class);
+                Intent intent = new Intent(MainActivity.this, Impacts.class);
                 startActivity(intent);
             }
         });
