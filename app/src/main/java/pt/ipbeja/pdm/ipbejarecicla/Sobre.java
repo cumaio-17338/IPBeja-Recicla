36f575eb0a76f34d3f088efa5d2c51330433b02e
jaciara <18582@stu.ipbeja.pt>
2021-02-06 19:24:23 +0000

    +Classe sobre.

---
+++ app/src/main/java/pt/ipbeja/pdm/ipbejarecicla/Sobre.java
@@ -0,0 +1,26 @@
+package pt.ipbeja.pdm.ipbejarecicla;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Sobre extends AppCompatActivity {
+
+    private TabLayout tabLayout; //to put the green toolbar
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+
+        //to put the green toolbar
+        Toolbar toolbar = findViewById(R.id.mainToolBar);
+        toolbar.setTitle(R.string.saber_mais);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
